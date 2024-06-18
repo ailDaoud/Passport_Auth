6eda597767c2edcda1a72e6732ef35b5a9aa4c0d
ailDaoud <daoudali259@gmail.com>
2024-06-18 18:33:00 +0300

    1.0

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware('auth:api');*/
+Route::post('auth/register',[UserController::class,'register']);
+Route::post('auth/login',[UserController::class,'login']);
